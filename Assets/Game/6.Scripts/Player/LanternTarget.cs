a251740273e02853e0899749bc6bf4349635790a
Rafael Farinha <raf.farinha@outlook.com>
2022-09-01 01:05:37 -0300

    Alvo da lanterna

--- Assets/Game/6.Scripts/Player/LanternTarget.cs
+++ Assets/Game/6.Scripts/Player/LanternTarget.cs
@@ -2,17 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum LanternTargetType
+{
+    soul, dredge, hiddenItem
+}
+
 public class LanternTarget : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float lanternProgress;
+    public float targetThreshold;
+    [SerializeField] private float lanternRecovery;
+    [SerializeField] private bool lanternActiveOver;
+    [SerializeField] private bool lanternImmune;
+
+    public void LanternGain(float effect)
     {
-        
+        if(lanternImmune)
+            return;
+
+        lanternActiveOver = true;
+        lanternProgress = Mathf.Clamp(lanternProgress + effect * Time.deltaTime, 0, targetThreshold);
+
+        if(lanternProgress >= targetThreshold)
+        {
+            LanternEffect();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LanternLoss()
     {
-        
+        if (lanternActiveOver)
+            return;
+
+        if (lanternProgress <= 0)
+            return;
+
+        lanternProgress = Mathf.Clamp(lanternProgress - lanternRecovery * Time.deltaTime, 0, targetThreshold);
+    }
+
+    public void LanternEffect()
+    {
+        lanternImmune = true;
+
+    }
+
+    private void Update()
+    {
+        LanternLoss();
     }
 }
