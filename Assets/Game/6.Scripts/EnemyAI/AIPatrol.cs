8315de78d163d054b65b96ff278ea8386c0ce44f
Lucas Ferreira Lima da Cunha <luckasvish@gmail.com>
2022-11-26 12:03:59 -0300

    PatrolNewBehaviour

--- Assets/Game/6.Scripts/EnemyAI/AIPatrol.cs
+++ Assets/Game/6.Scripts/EnemyAI/AIPatrol.cs
@@ -8,11 +8,14 @@ public class AIPatrol : MonoBehaviour
     Enemy entity;
     ActionStatus status = ActionStatus.Running;
     BehaviourState classState = BehaviourState.Patrol;
+    enum PatrolBehaviour { Circular, Sequential }
+    PatrolBehaviour actualBehaviour;
 
     [SerializeField] private Transform[] patrolWaypoints;
     private int waypointIndex;
 
     [SerializeField] private float moveSpeed;
+    private int sign;
 
     // Start is called before the first frame update
     void Start()
@@ -55,7 +58,13 @@ public class AIPatrol : MonoBehaviour
         transform.rotation = Quaternion.RotateTowards(transform.rotation, quaternion, 100*Time.deltaTime);
 
         if (Vector3.Distance(entity.transform.position, patrolWaypoints[waypointIndex].transform.position) == 0)
-            waypointIndex = ((waypointIndex + 1) % patrolWaypoints.Length);
+        {
+            waypointIndex = Mathf.Clamp(actualBehaviour == PatrolBehaviour.Circular ? waypointIndex + sign : waypointIndex + sign & patrolWaypoints.Length, 0, patrolWaypoints.Length);
+
+            if (waypointIndex == patrolWaypoints.Length || waypointIndex == 0)
+                sign *= actualBehaviour == PatrolBehaviour.Circular ? -1 : 1;
+            //waypointIndex = ((waypointIndex + 1) % patrolWaypoints.Length);
+        }
     }
 
     void ResetPatrol()
