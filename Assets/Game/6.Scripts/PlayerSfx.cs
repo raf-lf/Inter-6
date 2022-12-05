3696c1c747f2fdd199e25155a62514fbeaec867f
jphdm20 <72819815+jphdm20@users.noreply.github.com>
2022-12-05 19:44:02 -0300

    Motor corrigido

--- Assets/Game/6.Scripts/PlayerSfx.cs
+++ Assets/Game/6.Scripts/PlayerSfx.cs
@@ -3,22 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
 using FMOD;
+using System;
 
 public class PlayerSfx : MonoBehaviour
 {
-    public StudioEventEmitter sfxEngine;
     public static string explosionEventPath = "event:/SFX/explosion";
     public static string collisionEventPath = "event:/SFX/PLAYER/hit_boat";
 
+    public static FMOD.Studio.EventInstance sfxEngine;
     public static FMOD.Studio.EventInstance collisionEvent;
     public static FMOD.Studio.EventInstance explosionEvent;
 
     private void Awake()
     {
         explosionEvent = RuntimeManager.CreateInstance(explosionEventPath);
+        sfxEngine = RuntimeManager.CreateInstance("event:/SFX/PLAYER/boat_engine");
         collisionEvent = RuntimeManager.CreateInstance(collisionEventPath);
         
     }
+
+    public static void EngineOnOff(bool on) 
+    {
+        if (on == true)
+        {
+            sfxEngine.start();
+            sfxEngine.setParameterByName("rpm", 0.6f);
+
+        }
+        else
+        {
+
+            sfxEngine.setParameterByName("rpm", 0);
+            sfxEngine.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+          //  sfxEngine.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+
+        }
+        
+        
+        }
     
 
 }
